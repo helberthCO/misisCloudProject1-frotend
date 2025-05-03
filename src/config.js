8877cf5c84f529e634218b3662b1774c33a65d21
Helberth Caro <31378229+helberthCO@users.noreply.github.com>
2025-05-03 18:58:58 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const backendUrl = process.env.BACKEND_URL || 'http://34.69.143.161';
+export const backendUrl = process.env.BACKEND_URL || 'http://35.201.69.50';
 console.log(`Backend URL: ${backendUrl}`);
 
 export default {
